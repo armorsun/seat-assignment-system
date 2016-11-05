13637ae2a556b7b24e50a8a94478ae3ca77d0a24
lc1024 <b02611002@ntu.edu.tw>
2016-11-05 13:55:02 +0800

    done lcddisplay.h library defining

---
+++ LCDDisplay/LCDDisplay.ino
@@ -0,0 +1,31 @@
+//#include "lcddisplay.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Hi");
+//  LCDSayHello();
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hi! Joyce!");
+  lcd.setCursor(0,1);
+  lcd.print("Are you okay?");
+   lcd.setCursor(0,2);
+  lcd.print("Arduino LCM IIC 2004");
+   lcd.setCursor(2,3);
+  lcd.print("Power By Ec-yuan!");
+}
+
+
+void loop()
+{
+}
+
+
---
+++ LCDDisplay/lcddisplay.cpp
@@ -0,0 +1,8 @@
+#include "lcddisplay.h"
+#include "Arduino.h"
+
+void lcddisplay(){
+  Serial.begin(9600);
+  Serial.print('h');
+}
+
