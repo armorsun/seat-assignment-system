4939b6c527afbac875f5c9510074493e5420fae0
lc1024 <b02611002@ntu.edu.tw>
2016-11-07 01:13:16 +0800

    successfully compile in checkBtnPressed_dev

--- checkBtnPressed_dev/checkBtnPressed.cpp
+++ checkBtnPressed_dev/checkBtnPressed.cpp
@@ -1,8 +1,30 @@
-#include "checkBtnPressed.h"
+#define BTNPIN 6
+
 #include "Arduino.h"
+#include "checkBtnPressed.h"
+#include "LEDControl.h"
+#include "uploadData.h" //coming soon
+
+extern int seatStatus;
+extern int timeRemained;
 
 void checkBtnPressed(){
 
+  pinMode(BTNPIN, INPUT);
+  int buttonState = digitalRead(BTNPIN);
   
+  if (buttonState == HIGH) {    
+    
+    if(seatStatus == -1 || seatStatus == 0){
+      //if seat avaliable or temporarily out, do nothing, 
+      return;
+    }else if(seatStatus == 1){
+      //if occupied...
+      seatStatus = 0;       //1. set seatStatus temporarily out, 
+      LEDControl(0,0);      //2. turn on blue LED, 
+      timeRemained = 1800;  //3. start counting down 30 mins
+      uploadData();         //4. report to RPi
+    }
+  }
 }
 
