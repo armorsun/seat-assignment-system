6a4eada5346c9363c51aed4d726bd2235375217a
Fruit307 <sleet0718@gmail.com>
2016-12-16 21:19:38 +0800

    wrong word.

--- lcddisplay.cpp
+++ lcddisplay.cpp
@@ -34,7 +34,7 @@ void lcddisplay() {
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("[Avaliable]");
+    lcd.print("[Available]");
     lcd.setCursor(0, 1);
     lcd.print("Insert ID card");
 
