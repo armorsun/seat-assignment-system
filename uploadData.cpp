19013aa64a12ece469f730a3d4b820a7efeb96b5
Sunny <gsunny4178@gmail.com>
2016-12-30 21:01:08 +0800

    simplified upload_data

--- uploadData.cpp
+++ uploadData.cpp
@@ -10,17 +10,13 @@ extern int timeRemained;
 void uploadData() {
   wifiSerial.println("AT+CIPSTART=\"TCP\",\"140.112.94.163\",9489");
   delay(2000);
-  String UID=String((uint8_t)UIDStored[0])+String((uint8_t)UIDStored[1])+String((uint8_t)UIDStored[2])+String((uint8_t)UIDStored[3]);
-  Serial.println(UID);
-  Serial.println(String(seatStatus));
-  Serial.println(String(timeRemained));
-
-  String cmd = "GET /?id=" + UID + "&status=" + String(seatStatus) + "&time=" + timeRemained + " HTTP/1.1\r\n\r\n";
+  String uid = String(uint8_t(UIDStored[0])) + String(uint8_t(UIDStored[1])) + String(uint8_t(UIDStored[2])) + String(uint8_t(UIDStored[3]));
+  String cmd = "GET /?id=" + uid + "&status=" + String(seatStatus) + " HTTP/1.1\r\n\r\n";
   wifiSerial.print("AT+CIPSEND=");
   wifiSerial.println(cmd.length());
   wifiSerial.flush();
 
   delay(4000);
   wifiSerial.print(cmd);
-  Serial.println("send_ok");
 }
+
